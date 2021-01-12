47ef8ebbb1d743e92f03d8be73336a0c7fdcdf86
zn_lp <lipu@zgzhongnan.com>
2021-01-12 15:12:55 +0800

    优化

--- src/main/java/com/gupao/user/controller/TestController.java
+++ src/main/java/com/gupao/user/controller/TestController.java
@@ -1,11 +1,11 @@
 package com.gupao.user.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
@@ -15,11 +15,11 @@ import java.util.List;
 @RequestMapping("/user")
 public class TestController {
 
-    @Autowired
+    @Resource
     private DiscoveryClient discoveryClient;
 
     @RequestMapping("/test")
-    public List<ServiceInstance> findServiceInstance() throws Exception{
+    public List<ServiceInstance> findServiceInstance(){
         //查询指定服务名称下的所有实例的信息
         List<ServiceInstance> list=this.discoveryClient.getInstances("order");
         ServiceInstance serviceInstance=list.get(0);
@@ -29,7 +29,7 @@ public class TestController {
         return list;
     }
 
-    public void testUrl(String urlString){
+    private void testUrl(String urlString){
         URL url;
         try {
             url = new URL(urlString);
@@ -37,8 +37,9 @@ public class TestController {
             co.connect();
             System.out.println("连接可用");
         } catch (Exception e1) {
-            System.out.println("连接打不开!");
+
             url = null;
+            System.out.println("连接打不开!"+url);
         }
     }
 }
